95190bf578dd5b31558d9d52266a4166897e29e3
Shamsida <shamsidaali97@gmail.com>
2023-07-05 19:33:38 +0530

    added entity framework

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = "";
+
+        public int ContactNumber { get; set; }
+
+        public int Age { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Task.Data;
+
 namespace Task
 {
     public class Program
@@ -9,6 +12,7 @@ namespace Task
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Employee")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
